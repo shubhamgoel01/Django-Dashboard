c7aac97501d36fbf35fedc92f3475a2dc53e286c
Shubham Goel <33158343+shubhamgoel01@users.noreply.github.com>
2023-03-23 11:29:55 +0530

    Update beautyOpenssl.py

--- project1/testing/beautyOpenssl.py
+++ project1/testing/beautyOpenssl.py
@@ -2,17 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 def update():	
-    httpdURL='https://www.openssl.org/'
+    httpdURL='https://www.openssl.org/source/'
     resp=requests.get(httpdURL)
     if resp.status_code==200:
         print("Successfully opened the web page")
         print("The news are as follow :-\n")
         
         soup=BeautifulSoup(resp.text,'html.parser')        
-        l=soup.find("table",{"class":"newsflash"})
-        # print(l)
-        m = l.findAll("tr")[4]
-        print(m.text)           
+        # l=soup.find("div",{"class":"blog-index"})
+        l=soup.find("table")
+        m = l.findAll("tr")[2]
+        n = m.findAll("td")[2]        
+        print(n.text)
+        # m = l.findAll("tr")[5]
+        # print(m.text)           
     else:
         print('Error: %s' % resp.status_code)   
         
